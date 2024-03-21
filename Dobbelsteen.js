ed2b627d8306cbab057c08d18f35fd60c60998aa
Oscar <97081587@st.deltion.nl>
2024-03-21 09:56:56 +0100

    test

--- Dobbelsteen.js
+++ Dobbelsteen.js
@@ -1,3 +1,6 @@
-<script>
-    test
-</script>
+<><html>
+    <button onclick="random()">rol de dobbelsteen</button>
+</html><script>
+        function random() {Math.floor(Math.random() * 7)};
+        
+    </script></>
